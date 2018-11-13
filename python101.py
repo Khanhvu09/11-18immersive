e3f99410a04e5c57052d2e7def7c29078c984c6e
Khanh Vu <khanhvu@Khanhs-MacBook-Pro.local>
2018-11-13 14:27:25 -0500

    operators

--- python101.py
+++ python101.py
@@ -1,45 +1,80 @@
-print "Hello, World"
+# print "Hello, World"
 
-print """
-    It was a dark and stormy night.
-    A murder happened.
-"""
+# print """
+#     It was a dark and stormy night.
+#     A murder happened.
+# """
 
-print 'Hello, world'
+# print 'Hello, world'
 
-# Variables
-# - strings, letters, numbers, or any other stuff
-# you can make with a keyboard
-# a variable is just a fast way to refer to something else 
-# variables do not make the program faster.
-# They make the program slower!
-# Variables make it easier for us to write programs. 
+# # Variables
+# # - strings, letters, numbers, or any other stuff
+# # you can make with a keyboard
+# # a variable is just a fast way to refer to something else 
+# # variables do not make the program faster.
+# # They make the program slower!
+# # Variables make it easier for us to write programs. 
 
-# theBestClass = "the 11-18 immersive"
-# print theBestClass
+# # theBestClass = "the 11-18 immersive"
+# # print theBestClass
 
-# Data Types 
-# - Programming languages see different types of variables differently
-# - string - English stuff.
-# - Number - I think you know what this is. Something with numbers 
-# Floats, integers 
-# -- float = it has a . (decimal) in it 
-# -- integer = has no . (decimal) in it 
-# - Booleans = true or false, on or off, 1 or 0, yes or no, right or left 
-# - list - list of things. a signle variable with a bunch of parts 
-# - Dictonaries - variable of variables 
-# - Objects - super dictionaries 
+# # Data Types 
+# # - Programming languages see different types of variables differently
+# # - string - English stuff.
+# # - Number - I think you know what this is. Something with numbers 
+# # Floats, integers 
+# # -- float = it has a . (decimal) in it 
+# # -- integer = has no . (decimal) in it 
+# # - Booleans = true or false, on or off, 1 or 0, yes or no, right or left 
+# # - list - list of things. a signle variable with a bunch of parts 
+# # - Dictonaries - variable of variables 
+# # - Objects - super dictionaries 
 
-# Primitive Data types = string, number, boolean 
-month = "November";
-print type(month)
-date = 13
-print type(date)
-dateAsFloat = 13.0
-print type(dateAsFloat)
-aBool = True
-print type(aBool)
-aList = []
-print type(aList)
-aDictionary = {}
-print type(aDictionary)
+# # Primitive Data types = string, number, boolean 
+# month = "November";
+# print type(month)
+# date = 13
+# print type(date)
+# dateAsFloat = 13.0
+# print type(dateAsFloat)
+# aBool = True
+# print type(aBool)
+# aList = []
+# print type(aList)
+# aDictionary = {}
+# print type(aDictionary)
+
+# # concatenate is programming speak for add things together 
+# first = "Khanh" 
+# last = "Vu"
+# fullName = first + last 
+# fullName = first + " " + last
+# print fullName
+
+# fourteen = 10 + 4
+# print fourteen
+# fourteen = "10" + "4"
+# print fourteen
+# # fourteen = 10 + "4" (wont work)
+
+# # cast = change a variable to a new data type 
+# fourteen = int("10") + 4 
+
+# Math = +. -, *, % 
+# print 2 + 2
+# print 2 - 2
+# print 2 / 2
+# print 2 * 2
+# # % = modulus. Divides the number and gives you the remainder 
+# print 2 % 2 
+# print 2 % 3
+# print 2 ** 3
+# print "-" * 20 
+# A string and a * and a number = give me X strings
+
+# Python does not have an simple incrementer 
+# num = 1;
+# num++ 
+# num += 1
+# C 
+# C++
